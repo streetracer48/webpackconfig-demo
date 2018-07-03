bd16c8f313543639220ea427a3f3574906399925
wasifmiraz <wasifmiraz@mail.com>
2018-07-03 23:10:20 +0600

    added full config

--- webpack.config.js
+++ webpack.config.js
@@ -13,3 +13,23 @@ module.exports = {
 
     mode: 'development',
 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
+}
